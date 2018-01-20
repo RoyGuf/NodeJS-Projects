3675e12a97b3412de346149d5d97b74b6cdecb0e
RoyGuf <roi8880@gmail.com>
2018-01-20 20:53:00 +0200

    new

---
+++ routes/shorter.js
@@ -0,0 +1,34 @@
+var mongo = require("mongodb").MongoClient;
+var validUrl = require("valid-url");
+var dburl = 'mongodb://127.0.0.1:27017/mongopro'
+
+function shorty(req,res){
+  var url = req.params.url;
+  var results = {};
+  if(validUrl.isUri(url)){
+    mongo.connect(dburl, function(err, db){
+      if(err){console.log(err)}
+      var shortener = db.collection('shortener');
+      var random=[];
+      for(var i =0;i<4;i++){
+        random.push(Math.floor(Math.random()*9))
+      }
+      shortener.insert([{url: url, short: random.join('')}],function(){
+          results = {
+            original_url: url,
+            short_url: req.protocol+'://'+req.headers['host']+'/'+random.join('')
+          }
+		  db.close();
+		  res.send(results);
+		  
+      })
+	  
+    })
+	
+  }
+	
+
+}
+
+
+module.exports = shorty;
